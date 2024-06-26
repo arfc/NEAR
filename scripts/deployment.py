f6a8e6e1f2b78a8901a764e4fad38ead5a2bdeb9
Nathan Ryan <nsryan2@illinois.edu>
2024-06-26 12:50:51 -0500

    deployment pep updates

--- scripts/deployment.py
+++ scripts/deployment.py
@@ -8,7 +8,9 @@ def comm_by_prototype(evaler, time):
     """
     commission_df = evaler.eval('BuildSeries')
 
-    commission_df = commission_df.rename(index=str, columns={'EnterTime': 'Time'})
+    commission_df = commission_df.rename(
+        index=str,
+        columns={'EnterTime': 'Time'})
     commission_df = tools.add_missing_time_step(commission_df, time)
     commission_by_prototype = pd.pivot_table(
             commission_df,
@@ -25,7 +27,9 @@ def decom_by_prototype(evaler, commission_df, time):
     Track the number of decommissions by prototype.
     """
     decommission_df = evaler.eval('DecommissionSeries')
-    decommission_df = decommission_df.rename(index=str, columns={'ExitTime': 'Time'})
+    decommission_df = decommission_df.rename(
+        index=str,
+        columns={'ExitTime': 'Time'})
     decommission_df = tools.add_missing_time_step(decommission_df, time)
     decommission_by_prototype = pd.pivot_table(
             commission_df,
@@ -37,20 +41,26 @@ def decom_by_prototype(evaler, commission_df, time):
     decommission_df = decommission_df.drop('Count', axis=1)
     decommission_df = pd.concat([decommission_df, negative_count], axis=1)
     decommission_df.rename(columns={'ExitTime': 'Time'}, inplace=True)
-    decommission_by_prototype = decommission_df.pivot(index='Time', columns='Prototype')['Count'].reset_index()
+    decommission_by_prototype = decommission_df.pivot(index='Time',
+                                                       columns='Prototype'
+                                                       )['Count'].reset_index()
     decommission_by_prototype = decommission_by_prototype.fillna(0)
 
     return decommission_by_prototype
 
 
-def depl_by_prototype(commission_by_prototype, decommission_by_prototype, reactor_list):
+def depl_by_prototype(
+        commission_by_prototype,
+        decommission_by_prototype,
+        reactor_list):
     """
     Track deployments by prototype.
     """
     deployment_by_prototype = commission_by_prototype.copy()
 
     for reactor in reactor_list:
-        dep_react = decommission_by_prototype[reactor] + commission_by_prototype[reactor]
+        dep_react = decommission_by_prototype[reactor] + \
+            commission_by_prototype[reactor]
         deployment_by_prototype[reactor] = dep_react
 
     return deployment_by_prototype
@@ -62,6 +72,7 @@ def total_reactor(deployment_by_prototype, reactor_list):
     """
     for reactor in reactor_list:
         deployment_by_prototype[f'{reactor}_total'] = 0
-        deployment_by_prototype[f'{reactor}_total'] += deployment_by_prototype[reactor].cumsum()
+        deployment_by_prototype[f'{reactor}_total'] += \
+            deployment_by_prototype[reactor].cumsum()
 
     return deployment_by_prototype
